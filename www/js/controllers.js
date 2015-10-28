6cc6cca135167a8cf1557273ed0957326f469578
Victor Jansson <victor.jansson@casumo.com>
2015-10-28 18:08:21 +0100

    Adding modals for PSA values and potency check.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,27 @@
 angular.module('starter.controllers', [])
 
 .controller('StartCtrl', function($scope, $ionicModal) {
+  $scope.userState = {
+    firstVisit: true
+  }
 
-  $ionicModal.fromTemplateUrl('templates/modal-registration.html', {
+  $ionicModal.fromTemplateUrl('templates/modal-register-device.html', {
     scope: $scope,
     animation: 'slide-in-up'
   }).then(function(modal) {
-    $scope.modal = modal;
+    $scope.registerDeviceModal = modal;
 
-    modal.show();
+    if ($scope.userState.firstVisit) {
+      modal.show();  
+
+      $scope.userState.firstVisit = false;
+    }
+    
+
   });
 
-  $scope.openModal = function() {
-    $scope.modal.show();
-  };
-
-  $scope.closeModal = function() {
-    $scope.modal.hide();
+  $scope.openRegisterDeviceModal = function() {
+    $scope.registerDeviceModal.show();
   };
 
   //Cleanup the modal when we're done with it!
@@ -26,12 +31,41 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $ionicModal) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //
+  $ionicModal.fromTemplateUrl('templates/modal-psa-values.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.psaModal = modal;
+    });
+  
+  $ionicModal.fromTemplateUrl('templates/modal-potency-check.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.potencyModal = modal;
+    });
+
+
+  $scope.openPsaModal = function() {
+    $scope.psaModal.show();
+  }
+
+  $scope.openPotencyModal = function() {
+    $scope.potencyModal.show();
+  }
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    // @TODO only remove actuall modal
+    $scope.psaModal.remove();
+    $scope.potencyModal.remove();
+  });
 
 })
 
