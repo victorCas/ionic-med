4a9e4363329e0448407fc65c306ac1644714a7fa
Victor Jansson <victor.jansson@casumo.com>
2015-11-06 19:25:00 +0100

    Adding a simple survey example.

--- www/js/controllers/dashCtrl.js
+++ www/js/controllers/dashCtrl.js
@@ -20,8 +20,8 @@ app.controller('DashCtrl', function($scope, $ionicModal) {
       $scope.modals.symptonModal.show();
     }
 
-    $scope.openQuestionnareModal = function() {
-      $scope.modals.questionnareModal.show();
+    $scope.openSurveyModal = function() {
+      $scope.modals.surveyModal.show();
     }
 
 
@@ -56,11 +56,39 @@ app.controller('DashCtrl', function($scope, $ionicModal) {
           $scope.modals.symptonModal = modal;
         });
 
-      $ionicModal.fromTemplateUrl('templates/modal-questionnare.html', {
+      $ionicModal.fromTemplateUrl('templates/modal-survey.html', {
           scope: $scope,
           animation: 'slide-in-up'
         }).then(function(modal) {
-          $scope.modals.questionnareModal = modal;
+          var surveyModel = [
+            {
+              question: "How do you rate your confidence that you could get and keep an erection?",
+              answers: ['1 - Very Low', '2 - Low', '3 - Moderate', '4 - High', '5 - Very High']
+            },
+            {
+              question: "When you had erections with sexual stimulation, how often were your erections hard enough for penetration?",
+              answers: ['1 - Very Low', '2 - Low', '3 - Moderate', '4 - High', '5 - Very High']
+            },
+            {
+              question: "During sexual intercourse, how often were you able to mainten your erection after you had penetrated your partner",
+              answers: ['1 - Very Low', '2 - Low', '3 - Moderate', '4 - High', '5 - Very High']
+            }
+          ];
+
+          var activeQuestionIndex = 0;
+
+          $scope.modals.surveyModal = modal;          
+          $scope.activeQuestion = surveyModel[activeQuestionIndex];
+
+          $scope.nextQuestion = function() {
+              if (activeQuestionIndex < surveyModel.length - 1) {
+                activeQuestionIndex++;
+                $scope.activeQuestion = surveyModel[activeQuestionIndex];
+              } else {
+                $scope.modals.surveyModal.hide();
+              }
+          }
+
         });
     }
   })
